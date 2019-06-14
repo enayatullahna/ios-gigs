f58480726f4734b4085e6520cb4b35b3951449f0
Enayatullah Naseri <ena.nas@icloud.com>
2019-06-13 19:39:50 -0700

    Created Models, starting ModelController

---
+++ Gigs/Gigs/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Gigs
+//
+//  Created by Enayatullah Naseri on 6/13/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let username: String
+    let password: String
+    
+}
